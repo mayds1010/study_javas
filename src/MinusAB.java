ad89f24c74e809c948b9581693857a78c6738653
mayds1010@gmail.com <mayds1010@gmail.com>
2022-10-12 13:39:41 +0900

    Beakjoon

--- src/MinusAB.java
+++ src/MinusAB.java
@@ -1,6 +1,6 @@
 //문제
 //두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
-//refor:https://www.acmicpc.net/submit/1001/50431018
+//refer:https://www.acmicpc.net/submit/1001/50431018
 import java.util.Scanner;
 public class MinusAB {
 
