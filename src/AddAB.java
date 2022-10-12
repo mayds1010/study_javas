ed5c35cc6af0116245c40fc9d69304a4ecb3a8f3
mayds1010@gmail.com <mayds1010@gmail.com>
2022-10-12 13:39:12 +0900

    Beakjoon

--- src/AddAB.java
+++ src/AddAB.java
@@ -1,6 +1,6 @@
 //문제
 //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-//refor:https://www.acmicpc.net/submit/1000/50429027
+//refer:https://www.acmicpc.net/submit/1000/50429027
 import java.util.Scanner;
 public class AddAB{ //백준은 클래스이름을 메인으로 해야함
     public static void main(String[] args){
