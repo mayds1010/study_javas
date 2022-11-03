069e40fe52d2e5d6faa2bad0f65f83c0478c531c
mayds1010 <mayds1010@gmail.com>
2022-11-03 17:14:24 +0900

    coomit

--- src/Main.java
+++ src/Main.java
@@ -23,52 +23,31 @@
 
 //- try-on
 //- 실패 시 원인 기록
-// import java.util.Scanner;
-// public class Main {
-//     //두 정수 합 구함.
-//     public int plus(int input_first, int input_second) {
-//         int output_plus = input_first + input_second;
-//         return output_plus;
-//     }
-//     public static void main(String[] args) {
-//     //- 테스트 케이스
-//     // 3 + 5 =  8
-//     // 20 + 50 = 70
-//     // -1 + 5 = error 
-    
-//     // - 프로그래밍 순서
-//     // 입력
-//     Scanner sc = new Scanner(System.in);
-//     int input_first = sc.nextInt();
-//     int input_second = sc.nextInt();
-//     sc.close(); 
-    
-//     // 처리
-//     Main main = new Main();
-//     int output_plus = main.plus(input_first, input_second);
-    
-//     // 출력
-//         System.out.println(output_plus);
-//     }
-// }
 import java.util.Scanner;
 public class Main {
+    //두 정수 합 구함.
+    public int plus(int input_first, int input_second) {
+        int output_plus = input_first + input_second;
+        return output_plus;
+    }
     public static void main(String[] args) {
-        
-// 프로그래밍 순서
-// 입력
-Scanner sc = new Scanner(System.in);
-int T, A, B;
-T = sc.nextInt();
-for (int i = 0; i < T; i++) {
-    A = sc.nextInt();
-    B = sc.nextInt();
-
-// 처리
-int output = A+B;
-// 출력
-System.out.println(output);
-}
-sc.close();
-}
+    //- 테스트 케이스
+    // 3 + 5 =  8
+    // 20 + 50 = 70
+    // -1 + 5 = error 
+    
+    // - 프로그래밍 순서
+    // 입력
+    Scanner sc = new Scanner(System.in);
+    int input_first = sc.nextInt();
+    int input_second = sc.nextInt();
+    sc.close(); 
+    
+    // 처리
+    Main main = new Main();
+    int output_plus = main.plus(input_first, input_second);
+    
+    // 출력
+        System.out.println(output_plus);
+    }
 }
