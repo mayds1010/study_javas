cb1ffd140d070422a92e6402ca77b84dbd72060d
mayds1010 <mayds1010@gmail.com>
2022-11-04 11:27:46 +0900

    commit

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 |--- |---|---|---|---|
 | 사칙연산 | 덧셈 |[java](https://www.acmicpc.net/source/51273355) | [java](https://www.acmicpc.net/source/51273423) | 두 정수를 합함 |
 | 사칙연산 | 덧셈 |[java](https://www.acmicpc.net/source/51275879) | [java](https://www.acmicpc.net/source/51276729) | 케이스마다 A+B를 출력(반복) |
+| 사칙연산 | 뺄셈 |[java](https://www.acmicpc.net/source/50431018) | [java](https://www.acmicpc.net/source/51305930) | A-B를 출력 |
 
 ## 작성 소스
 
--- src/MinusAB.java
+++ src/MinusAB.java
@@ -5,17 +5,23 @@
 //refer:https://www.acmicpc.net/submit/1001/50431018
 import java.util.Scanner;
 public class MinusAB {
-
+    public int minus(int A,int B){
+        int output_minus = A-B;
+        return output_minus;
+    }
+//두 정수 마이너스구함
 public static void main(String[] args) {
 //입력
 //첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
     Scanner sc = new Scanner(System.in);
     int A = sc.nextInt();
     int B = sc.nextInt();
-
+    //처리
+    MinusAB main = new MinusAB();
+    int output_minus =main.minus(A, B);
 //출력
 //첫째 줄에 A-B를 출력한다.
-    System.out.println(A-B);
+    System.out.println(output_minus);
     sc.close();
  }
 }
